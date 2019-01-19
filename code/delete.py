aca1a1aba1cdec2e4d0fe1674c307c831e0780fe
Chenhuaqi6 <466940759@qq.com>
2019-01-19 14:53:30 +0800

    n

--- code/delete.py
+++ code/delete.py
@@ -1,17 +1,18 @@
-#删除数据
-import pymongo
+# #删除数据
+# import pymongo
 
-conn = pymongo.MongoClient("localhost",27017)
+# conn = pymongo.MongoClient("localhost",27017)
 
-dblist = conn.database_names()
+# dblist = conn.database_names()
 
-db = "stu"
-if db in dblist:
-    mydb = conn["stu"]
-    mycol = mydb["acct"]
-    myquery = {"acct_no":"622345888","acct_name":"Max"}
-    ret = mycol.delete_one(myquery)
-    print("删除笔数:%d" % ret.deleted_count)
-else:
-    print("db not found")
-conn.close()
+# db = "stu"
+# if db in dblist:
+#     mydb = conn["stu"]
+#     mycol = mydb["acct"]
+#     myquery = {"acct_no":"622345888","acct_name":"Max"}
+#     ret = mycol.delete_one(myquery)
+#     print("删除笔数:%d" % ret.deleted_count)
+# else:
+#     print("db not found")
+# conn.close()
+
